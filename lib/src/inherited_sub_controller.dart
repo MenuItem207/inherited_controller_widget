9e777dfa9cce1fcaf6768e8759c270245a3cf5bf
Emmanuel <70802541+MenuItem207@users.noreply.github.com>
2023-06-05 18:01:10 +0800

    Update inherited_sub_controller.dart

--- lib/src/inherited_sub_controller.dart
+++ lib/src/inherited_sub_controller.dart
@@ -1,15 +1,7 @@
-import 'package:flutter/material.dart';
 import 'package:inherited_controller_widget/inherited_controller_widget.dart';
 
 /// the controller stored within [InheritedControllerWidget]
 abstract class InheritedSubController {
   /// override this function to initialise the [InheritedSubController]
   Future init() async {}
-
-  /// returns the controller of type [T]
-  static T getController<T extends InheritedSubController>(
-    BuildContext context,
-  ) {
-    return InheritedControllerWidget.getController(context);
-  }
 }
